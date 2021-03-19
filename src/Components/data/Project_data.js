3784a55cae2ac65802bd54697c34564393538327
AyselHuseynovaa <ayselhuseynov3@gmail.com>
2021-03-20 02:28:30 +0400

    portfolio

---
+++ src/Components/data/Project_data.js
@@ -0,0 +1,66 @@
+// import React from "react";
+import WebPage from "../../logo/WebPage.png";
+import Alpenlite from "../../logo/Untitled.png";
+// import KpK from "../../logo/KpK.jpg";
+import EnRoute from "../../logo/EnRoute.png";
+import web_Page from "../../logo/web_Page.png";
+import Getaquote from "../../logo/Get a quote.png";
+import Password_generator from "../../logo/Password_generator.png";
+import Scandinavian from "../../logo/Scandinavian.png";
+import portfolio from "../../logo/portfolio.png"
+
+export const data_projects = [
+
+
+  {
+    name: "Portfolio",
+    image: portfolio,
+    deployed_url: "http://127.0.0.1:5500/index.html",
+    github_url: "https://github.com/AyselHuseynovaa/Final-project",
+  },
+  {
+    name: "Scandinavian",
+    image: Scandinavian,
+    deployed_url: "https://scandinavian-page.netlify.app/",
+    github_url: "https://github.com/AyselHuseynovaa/Final-project",
+  },
+  {
+    name: "WebPage",
+    image: WebPage,
+    deployed_url: "http://127.0.0.1:5500/webpage.html",
+    github_url: "https://github.com/AyselHuseynovaa/WebPage",
+  },
+  {
+    name: "Password_generator",
+    image: Password_generator,
+    deployed_url: "https://generate-pswwrd.netlify.app/",
+    github_url: "https://github.com/AyselHuseynovaa/Password-generator",
+  },
+  {
+    name: "Get a quote",
+    image: Getaquote,
+    deployed_url: "https://get-aquote.netlify.app/",
+    github_url: "https://github.com/AyselHuseynovaa/Final-task1",
+  },
+  {
+    name: "EnRoute",
+    image: EnRoute,
+    deployed_url: "https://fervent-mclean-2bd7d0.netlify.app/",
+    github_url: "https://github.com/AyselHuseynovaa/Final-task3",
+  },
+ 
+  {
+    name: "Alpenlite",
+    image: Alpenlite,
+    deployed_url: "https://alpenlite-5.netlify.app/",
+    github_url: "https://github.com/AyselHuseynovaa/Final-task5",
+  },
+
+  {
+    name: "web_Page",
+    image: web_Page,
+    deployed_url: "https://lucid-shirley-b709e2.netlify.app/",
+    github_url: "https://github.com/AyselHuseynovaa/Final-task2",
+  },
+];
+// export const data=[]
